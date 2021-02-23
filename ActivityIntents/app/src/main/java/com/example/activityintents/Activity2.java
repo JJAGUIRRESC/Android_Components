b68d0038437cebef3794db7567adb2dcb2a65c1e
JJAGUIRRESC <jjaguirresc@gmail.com>
2021-02-23 10:32:24 +0000

    Added ActivityIntents

---
+++ ActivityIntents/app/src/main/java/com/example/activityintents/Activity2.java
@@ -0,0 +1,41 @@
+package com.example.activityintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Activity2 extends AppCompatActivity {
+
+    Button btnActivity3;
+    Button btnSaySomething;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        btnActivity3 = findViewById(R.id.btnGoToActivity3);
+
+        // Definimos el listener
+        btnActivity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openActivity3 = new Intent(Activity2.this, Activity3.class);
+                startActivity(openActivity3);
+            }
+        });
+
+        btnSaySomething = findViewById(R.id.btnSaySomething);
+
+        btnSaySomething.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Activity2.this, "Hello!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ ActivityIntents/app/src/main/java/com/example/activityintents/ManageOuterAlarmsActivity.java
@@ -0,0 +1,14 @@
+package com.example.activityintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageOuterAlarmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_outer_alarms);
+    }
+}
